b81914a3c953701c1dd74d88a6d022aace750728
Pratik Hajare <pratikhajare09@gmail.com>
2023-12-25 10:39:25 +0530

    Initial commit

---
+++ src/main/java/com/hp/quizapigateway/QuizapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hp.quizapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizapigatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hp/quizapigateway/QuizapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.quizapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
